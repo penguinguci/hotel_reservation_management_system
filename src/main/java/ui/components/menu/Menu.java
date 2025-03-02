bb0432324d5b79417042c5c98fef951f0877dbbf
Trần Vũ <vutkd23405@gmail.com>
2025-03-02 09:51:57 +0700

    Complete menu building.

---
+++ src/main/java/ui/components/menu/Menu.java
@@ -0,0 +1,139 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ui.components.menu;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+import org.hibernate.Length;
+
+public class Menu extends JComponent {
+    private MenuEvent event;
+    private MigLayout layout;
+    private String[][] menuItems = new String[][] {
+        {"Dashboard"},
+        {"Email", "Inbox", "Read", "Compost"},
+        {"Chat"},
+        {"Calendar"},
+        {"UI Kit", "Accordion", "Alearts", "Badges", "Breadcrumbs", "Buttons", "Button group"}
+    };
+    
+    public Menu() {
+        init();
+    }
+
+    public MenuEvent getEvent() {
+        return event;
+    }
+
+    public void setEvent(MenuEvent event) {
+        this.event = event;
+    }
+    
+    public void init() {
+        layout = new MigLayout("wrap 1, fillx, gapy 0, inset 2", "fill");
+        setLayout(layout);
+        setOpaque(true);
+        
+        // Init MenuItem
+        for (int i = 0; i < menuItems.length; i++) {
+            addMenu(menuItems[i][0], i);
+        }
+    }
+    
+    private Icon getIcon(int index) {
+        String path = "src/main/java/ui/components/menu/" + index + ".png";
+        java.io.File file = new java.io.File(path);
+        if (file.exists()) {
+            return new ImageIcon(file.getAbsolutePath());
+        } else {
+            return null;
+        }
+    }
+    
+    private void addMenu(String menuName, int index) {
+        int length = menuItems[index].length;
+        MenuItem item = new MenuItem(menuName, index, length > 1);
+        Icon icon = getIcon(index);
+        if (icon != null) {
+            item.setIcon(icon);
+        }
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (length > 1) {
+                    if (!item.isSelected()) {
+                        item.setSelected(true);
+                        addSubMenu(item, index, length, getComponentZOrder(item));
+                    } else {
+                        // hide menu
+                        hideMenu(item, index);
+                        item.setSelected(false);
+                    }
+                } else {
+                    if (event != null) {
+                        event.selected(index, 0);
+                    }
+                }
+            }
+        });
+        add(item);
+        revalidate();
+        repaint();
+    }
+     
+    private void addSubMenu(MenuItem item, int index, int length, int indexZorder) {
+        JPanel panel = new JPanel(new MigLayout("wrap 1, fillx, inset 0, gapy 0", "fill"));
+        panel.setName(index + "");
+        panel.setBackground(new Color(176, 184, 181));
+        for (int i = 1; i < length; i++) {
+            MenuItem subItem = new MenuItem(menuItems[index][i], i, false);
+            subItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (event != null) {
+                        event.selected(index, subItem.getIndex());
+                    }
+                }
+            });
+            subItem.initSubMenu(i, length);
+            panel.add(subItem);
+        }
+        add(panel, "h 0!", indexZorder + 1);
+        revalidate();
+        repaint();
+        MenuAnimation.showMenu(panel, item, layout, true);
+    }
+    
+    private void hideMenu(MenuItem item, int index) {
+        for (Component com : getComponents()) {
+            if (com instanceof JPanel && com.getName() != null && com.getName().equals(index + "")) {
+                com.setName(null);
+                MenuAnimation.showMenu(com, item, layout, false);
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setColor(new Color(149, 145, 239));
+        g2.fill(new Rectangle2D.Double(0, 0, getWidth(), getHeight()));
+        super.paintComponent(g);
+    }
+    
+}
